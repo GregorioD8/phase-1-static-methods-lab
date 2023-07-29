bf205a3629def6b24f5604f3684e93653937ce7b
Gregorio <Greg.del801@gmail.com>
2023-07-29 10:38:26 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.slice(0, 1).toUpperCase() + string.slice(1, string.length)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+    
+  }
+  static titleize(sentence){
+    let exceptions = [ 'a', 'an', 'and', 'at', 'but', 'by', 'of', 'the', 'for', 'from']
+    let result = []
+    let wordsInSentence = sentence.split(' ')
+    for(let el of wordsInSentence){
+     if(!exceptions.includes(el) || el === wordsInSentence[0]){
+        el = this.capitalize(el)
+      } 
+      result.push(el)
+    }
+    return result.join(' ')
+  }
 }
+
